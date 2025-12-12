0f16ba98f8c70c9c46d9d3ada6a2224bcf5ca440
Yan <oliveirayan683@gmail.com>
2025-12-12 17:17:14 -0300

    first commit: api crud implementation and database integration

---
+++ spring-api-rest/src/main/java/com/example/spring_api_rest/SpringApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiRestApplication.class, args);
+	}
+
+}
---
+++ spring-api-rest/src/main/java/com/example/spring_api_rest/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.example.spring_api_rest.repositories;
+
+import com.example.spring_api_rest.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
